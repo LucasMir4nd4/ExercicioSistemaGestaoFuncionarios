7118b65426ea86f687da5fecc4f7351b802eee8d
Lucas de Albuquerque Miranda <lucas.a.miranda.dev@gmail.com>
2025-05-21 23:01:14 -0300

    primeiro

---
+++ src/entities/Funcionario.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+    private int idade;
+
+    public Funcionario() {
+    }
+
+
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
